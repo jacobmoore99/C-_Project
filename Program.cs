b2e6209e4160ce2104fdb4b97a04178f9b447082
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2021-06-01 20:32:42 -0400

    Second

--- Program.cs
+++ Program.cs
@@ -12,6 +12,17 @@ namespace C__Project
             Console.WriteLine("What is your name?");
             name = Console.ReadLine();
             Console.WriteLine("What's up, " + name );
+            string answer;
+            Console.WriteLine("Want to see a school math problem " + name + "?");
+            answer = Console.ReadLine();
+            Console.WriteLine(answer + "? Ok.");
+            int num1 = 12;
+            int num2 = 14;
+            int num3;
+
+            num3 = num1 + num2;
+
+            Console.WriteLine(num3);
 
         }
     }
