c4002cf73a45e88658e309ff1565879a964cfeb2
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2021-06-02 12:35:55 -0400

    Third

---
+++ Basic_Math.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace C__Project
+{
+    class Basic_Math
+    {
+        static void Second(string[] args)
+        {
+            string greeting = "Hello everyone";
+            Console.WriteLine(greeting);
+            string name;
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What's up, " + name );
+            string answer;
+            Console.WriteLine("Want to see a cool math problem " + name + "?");
+            answer = Console.ReadLine();
+            Console.WriteLine(answer + "? Ok.");
+            int num1 = 12;
+            int num2 = 14;
+            int num3;
+
+            num3 = num1 + num2;
+
+            Console.WriteLine(num3);
+
+        }
+    }
+}
