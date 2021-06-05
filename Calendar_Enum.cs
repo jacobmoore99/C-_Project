39b79760fffefa487ac27d6c36c3ab8687ed84d6
Jacob Moore <jacob.moore@GCCYJQ72J1WK.local>
2021-06-04 20:59:58 -0400

    Twelfth

---
+++ Calendar_Enum.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace C__Project
+{
+    class Calendar_Enum
+    {
+      enum Months
+    {
+      None,
+      January,    // 1
+      February,   // 2
+      March,      // 3
+      April,      // 4
+      May,        // 5
+      June,       // 6
+      July,        // 7
+      August,
+      September,
+      October,
+      November,
+      December
+    }
+
+    enum Days
+    {
+      None,
+      Monday,    // 1
+      Tuesday,   // 2
+      Wednesday,      // 3
+      Thursday,      // 4
+      Friday,        // 5
+      Saturday,       // 6
+      Sunday       // 7
+      
+    }
+    static void Twelfth(string[] args)
+    {
+      Random rnd = new Random();
+      int day  = rnd.Next(1, 8);  // creates a number between 1 and 12
+      int month   = rnd.Next(1, 13);   // creates a number between 1 and 6
+      int date   = rnd.Next(1, 32);     // creates a number between 0 and 51
+
+      Console.WriteLine("\nYour numerical calendar date is: " + day.ToString() + ", " + month.ToString() + " " + date.ToString());
+      Console.WriteLine("Use this program to perform a conversion.");
+      
+      Console.WriteLine("\nEnter the first number from 1-7.");
+      string dnum;
+      dnum = Console.ReadLine();
+      var intd = int.Parse(dnum);
+
+      Console.WriteLine("\nEnter the first number from 1-12.");
+      string mnum;
+      mnum = Console.ReadLine();
+      var intm = int.Parse(mnum);
+
+      Console.WriteLine("\nEnter the first number from 1-31.");
+      string tnum;
+      tnum = Console.ReadLine();
+
+      var doutput = (Days)intd;
+      var moutput = (Months)intm;
+      Console.WriteLine("\nThe date is: " + doutput + ", " + moutput + " " + tnum + ".\n");
+    }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,29 +1,65 @@
 ﻿using System;
-using System.IO;
-using System.Threading.Tasks;
 
 namespace C__Project
 {
     class Program
     {
-        static void Main()
+      enum Months
     {
-        int counter = 0;  
-        string line;  
-        System.Console.WriteLine("\n");
-        // Read the file and display it line by line.  
-        System.IO.StreamReader file =
-            new System.IO.StreamReader(@"TextFileTest1.txt");  
-        while((line = file.ReadLine()) != null)  
-        {  
-            System.Console.WriteLine(line);  
-            counter++;  
-        }  
-          
-        file.Close();  
-        System.Console.WriteLine("There were {0} lines in this text file.", counter);  
-        // Suspend the screen.  
-        System.Console.ReadLine();  
+      None,
+      January,    // 1
+      February,   // 2
+      March,      // 3
+      April,      // 4
+      May,        // 5
+      June,       // 6
+      July,        // 7
+      August,
+      September,
+      October,
+      November,
+      December
+    }
+
+    enum Days
+    {
+      None,
+      Monday,    // 1
+      Tuesday,   // 2
+      Wednesday,      // 3
+      Thursday,      // 4
+      Friday,        // 5
+      Saturday,       // 6
+      Sunday       // 7
+      
+    }
+    static void Main(string[] args)
+    {
+      Random rnd = new Random();
+      int day  = rnd.Next(1, 8);  // creates a number between 1 and 12
+      int month   = rnd.Next(1, 13);   // creates a number between 1 and 6
+      int date   = rnd.Next(1, 32);     // creates a number between 0 and 51
+
+      Console.WriteLine("\nYour numerical calendar date is: " + day.ToString() + ", " + month.ToString() + " " + date.ToString());
+      Console.WriteLine("Use this program to perform a conversion.");
+      
+      Console.WriteLine("\nEnter the first number from 1-7.");
+      string dnum;
+      dnum = Console.ReadLine();
+      var intd = int.Parse(dnum);
+
+      Console.WriteLine("\nEnter the first number from 1-12.");
+      string mnum;
+      mnum = Console.ReadLine();
+      var intm = int.Parse(mnum);
+
+      Console.WriteLine("\nEnter the first number from 1-31.");
+      string tnum;
+      tnum = Console.ReadLine();
+
+      var doutput = (Days)intd;
+      var moutput = (Months)intm;
+      Console.WriteLine("\nThe date is: " + doutput + ", " + moutput + " " + tnum + ".\n");
     }
     }
 }
